6fcfe6af091c2bb8b80fb4b8439fb5b7f47b019b
Theovain <91465683+Theovain@users.noreply.github.com>
2022-06-03 12:43:11 +0200

    Modificacion ClienteOM y ClienteRepositoryIMPLTesting
    Eliminados los metodos getSocios, getAbonados y getEsporadicos. Modificado ligeramente ClienteRepositoryIMPLTesting

--- src/repositories/ClienteRpository.java
+++ src/repositories/ClienteRpository.java
@@ -15,7 +15,14 @@ public interface ClienteRpository {
      * @param identificador;
      * @return Lista de clientes que coincidan con el identificador introducido;
      */
+	
     public ArrayList<Cliente> getClientes(String identificador);
+    
+    
+    
+    
+    
+    
     /**
      * Este metodo de identificar por matrícula buscará en la lista de clientes un cliente
      * culla matrícula coincida con la matrícula introducida por parametro.
@@ -25,9 +32,15 @@ public interface ClienteRpository {
      * @param matricula; Matrícula introducida para realizar la búsqueda
      * @return Obejto de tipo Socio culla matricula coincida con la intorucida por parametro;
      */
-    
+
     public Cliente IdentifyByMatricula(String matricula);
 
+    
+    
+    
+    
+    
+    
     /**
      * Este metodo será llamado en caso de que el metodo de identificar matriculas no encuentre ninguna coincidencia.
      * A demas el parametro de matricula de este metodo es una reutilizacion del parametro matricula del metodo de buscar por matriculas.
@@ -35,16 +48,30 @@ public interface ClienteRpository {
      * @param matricula; Matricula necesaria para generar el estacionamiento
      * @return ; Un objeto de tipo Estacionamiento (usando datos por ahora inventados);
      */
+    
     public Estacionamiento generarEstacionamiento(String matricula);
 
+    
+    
+    
+    
+    
+    
     /**
      * Este metodo se encargara de efectuar el pago del estacionamiento asociado a una matrícula.(si no hay ningún estacionamiento asociado a esta matrícula se mostrara un mensaje de error informando al cliente).
      *
      * @param metodoPago; Metodo de pago de la persona que genero el estacionamiento para efectuar el pago.
      * @return true/false; Devolvera true si el pago se ha efectuado y false si no se ha podido efectuar el pago
      */
+    
     public boolean pagarEstacionamiento(String metodoPago);
 
+   
+    
+    
+    
+    
+    
     /**
      * En el caso de que el metodo de pagar estacionamiento devuelva true, este metodo generara un nuevo objeto de estacionamientoFInalizado con los datos del estacionamiento asociado a la matricula introducida.
      * (si valor de "statusEstacionamiento" esta en true significa que esta en curso, si esta en false significa que esta finalizado)
@@ -53,5 +80,6 @@ public interface ClienteRpository {
      * @param matricula; Para buscar el estacionamiento asociado a esa matricula.
      * @return ; Un objeto de la clase EstacionamientoFinalizado.
      */
+    
     public EstacionamientoFinalizado finalizarEstacionamiento(String matricula);
 }
