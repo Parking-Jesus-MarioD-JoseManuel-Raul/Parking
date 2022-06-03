3434b0e0541adda196319e9943053b4af79d29b1
JoseManuelVeraGordillo <95566034+JoseManuelVeraGordillo@users.noreply.github.com>
2022-06-03 12:56:39 +0200

    Prueba01 Vera
    cambiamos  un estacionamientoOM

--- src/objectMother/EstacionamientoOM.java
+++ src/objectMother/EstacionamientoOM.java
@@ -1,4 +1,27 @@
 package objectMother;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Estacionamiento;
+
 public class EstacionamientoOM {
+
+public static List<Estacionamiento> getEstacionamiento(){
+	LocalDateTime horaEntrada []= {LocalDateTime.now().plusMinutes(20)};
+
+	String[] plate= {"124HYT", "842YRO", "942UER", "154OPS", "353JSF", "812JQA", "123FAE", "OIO125", 
+	 "031OOD"};
+	
+	
+	
+	ArrayList<Estacionamiento> estacionamientos = new ArrayList<>();
+	
+	for (int i = 0; i < plate.length; i++) {
+		estacionamientos.add(new Estacionamiento(horaEntrada[i],plate[i]));
+	}
+	
+return estacionamientos;	
+}
 }
