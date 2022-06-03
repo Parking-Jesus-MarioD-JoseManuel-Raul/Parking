cf9afec207ebd00d5957fbc425b520a9f7b7a490
JMCR9815 <38430152+JMCR9815@users.noreply.github.com>
2022-06-03 13:19:58 +0200

    EstacionamientoRepositoryIMPL

---
+++ src/tests/EstacionamientoRepositoryIMPLTest.java
@@ -0,0 +1,41 @@
+package tests;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * EstacionamientoRepositoryIMPL Tester.
+ *
+ * @author <Jesus Macarro>
+ * @version 1.0
+ * @since <pre>jun. 3, 2022</pre>
+ */
+public class EstacionamientoRepositoryIMPLTest {
+
+    @Before
+    public void before() {
+    }
+
+    @After
+    public void after() {
+    }
+
+    /**
+     * Method: getEstacionamientosOperativos()
+     */
+    @Test
+    public void testGetEstacionamientosOperativos() {
+//TODO: Test goes here... 
+    }
+
+    /**
+     * Method: getEstacionamientosFinalizados()
+     */
+    @Test
+    public void testGetEstacionamientosFinalizados() {
+//TODO: Test goes here... 
+    }
+
+
+} 
