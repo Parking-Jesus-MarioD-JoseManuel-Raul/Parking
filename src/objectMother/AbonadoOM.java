fe0a55a854d61ea965c261ff93093b5686827c03
JMCR9815 <38430152+JMCR9815@users.noreply.github.com>
2022-06-08 12:21:50 +0200

    Modificaciones de AbonadoOM.java y AbonadoRepositoryIMPLTest.java

--- src/objectMother/AbonadoOM.java
+++ src/objectMother/AbonadoOM.java
@@ -13,7 +13,7 @@ public class AbonadoOM {
         ArrayList<Bono> listaBonos = BonoOM.getBonosList();
         ArrayList<Estacionamiento> estacionamientos = (ArrayList<Estacionamiento>) EstacionamientoOM.getEstacionamiento();
         for (int i = 0; i < 8; i++) {
-            listaAbonados.add(new Abonado(Identificador.Plastico, clientes.get(i).getMatricula(), clientes.get(i).getPayMethod(), listaBonos.get(random.nextInt(3)), estacionamientos.get(i)));
+            listaAbonados.add(new Abonado(Identificador.Plastico, clientes.get(i).getMatricula(), clientes.get(i).getPayMethod(), listaBonos.get(random.nextInt(3)), estacionamientos.get(random.nextInt(estacionamientos.size()))));
         }
 
         return listaAbonados;
