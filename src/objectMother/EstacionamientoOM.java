5b2ba522b479ca5fc324b1baa73a481d9c71a84d
JoseManuelVeraGordillo <95566034+JoseManuelVeraGordillo@users.noreply.github.com>
2022-06-08 12:15:05 +0200

    prueba desde Visual

--- src/objectMother/EstacionamientoOM.java
+++ src/objectMother/EstacionamientoOM.java
@@ -9,7 +9,9 @@ import model.Estacionamiento;
 public class EstacionamientoOM {
 
 public static List<Estacionamiento> getEstacionamiento(){
-	LocalDateTime horaEntrada []= {LocalDateTime.now().plusMinutes(20)};
+	LocalDateTime horaEntrada []= {LocalDateTime.of(2022, 06, 8, 10, 05),LocalDateTime.of(2022, 06, 8, 11, 05),LocalDateTime.of(2022, 06, 8, 12, 05),
+		LocalDateTime.of(2022, 06, 8, 13, 05),LocalDateTime.of(2022, 06, 8, 14, 05),LocalDateTime.of(2022, 06, 8, 15, 05),
+		LocalDateTime.of(2022, 06, 8, 16, 05),LocalDateTime.of(2022, 06, 8, 17, 05),LocalDateTime.of(2022, 06, 8, 18, 05)};
 
 	String[] plate= {"124HYT", "842YRO", "942UER", "154OPS", "353JSF", "812JQA", "123FAE", "OIO125", 
 	 "031OOD"};
